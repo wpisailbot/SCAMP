17d8acd42361d5213f3cf809bb047f06e65920b7
jsburklund <robomower9508@gmail.com>
2018-04-07 21:59:22 -0400

    Control rudder only, and don't send out winch positions

--- SCAMP.ino
+++ SCAMP.ino
@@ -1,7 +1,6 @@
-
 #include "mcp_can.h"
 #include <SPI.h>
-#include <Servo.h>
+#include <ServoTimer2.h>
 
 #define CAN_CS    A2    // ATMEGA Pin 25, PC2, ADC2
 #define CAN_SCK   13    // ATMEGA Pin 19, PB5
@@ -17,9 +16,9 @@
 
 // #define MTR_INA     // MTR_2, ATMEGA Pin 9, PB6
 // #define MTR_INB     // MTR_3, ATMEGA Pin 10, PB7
-#define MTR_PWMA  10  // MTR_1, ATMEGA Pin 16, PB2
-#define MTR_PWMB  9   // MTR_0, ATMEGA Pin 15, PB1
-#define MTR_CS    4   // MTR_C, ATMEGA Pin 6, PD4
+//#define MTR_PWMA  10  // MTR_1, ATMEGA Pin 16, PB2
+//#define MTR_PWMB  9   // MTR_0, ATMEGA Pin 15, PB1
+//#define MTR_CS    4   // MTR_C, ATMEGA Pin 6, PD4
 
 
 #define ANL_0   A0  // ATMEGA Pin 23, PC0, ADC0
@@ -29,18 +28,32 @@
 #define DIG_2   3   // ATMEGA Pin 5, PD3, INT1
 #define DIG_3   2   // ATMEGA Pin 4, PD2, INT0
 
+#define PIN_FWD 8   //PB0
+#define PIN_BWD 7   //PD7
+#define PIN_PWM 10   //PB2
+#define PIN_CURR 5  //ADC5
+#define CURRENT_SENSE_SCALE 1
+#define T_DWN1 1
+#define T_DWN2 1
+
 // #define SPARE      // Spare pin on ATMEGA pin 14, PB0
 
 // Initialize these methods
 void initCAN();
 int loopCAN();
 int sendCANMessage(byte data[], int dataLength);
-
+void initMotor(uint8_t fwd_pin, uint8_t bwd_pin, uint8_t pwm_pin, uint8_t curr_pin, uint8_t fault1_pin, uint8_t fault2_pin);
+void setMotor(int8_t power);
+uint8_t isMotorFault();
+float getCurrent();
+uint8_t parseInt(int16_t* val);
 
 
 int step_time = 50;
 long last_time = millis() + step_time;
 int count = 0;
+
+// Initialize stuff for motor controller
 
 
 // Initialize CAN Stuff
@@ -53,16 +66,16 @@ MCP_CAN CAN0(CAN_CS);     // Set CS to pin 10
 
 const int kWinchPort = DIG_1;
 const int kRudderPort = DIG_2;
-Servo winch, rudder;
+ServoTimer2 winch, rudder;
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("Testing");
   analogReference(DEFAULT);
-  winch.attach(kWinchPort);
+  //winch.attach(kWinchPort);
   rudder.attach(kRudderPort);
-  winch.write(90);
-  rudder.write(90);
+  //winch.write(DEFAULT_PULSE_WIDTH);
+  rudder.write(DEFAULT_PULSE_WIDTH);
 
   // Initialize LEDs
   pinMode(LED_0, OUTPUT);
@@ -74,6 +87,7 @@ void setup(){
   digitalWrite(LED_1, LOW);
 
   initCAN();
+  //initMotor(PIN_FWD, PIN_BWD, PIN_PWM, PIN_CURR, T_DWN1, T_DWN2);
 }
 
 byte tempData[8] = {0x00, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -85,11 +99,20 @@ void loop(){
     Serial.println("New messages found");
   }
 
+  int16_t my_power = 0;
+  //Read Serial values for motor power testing
+  /*if(parseInt(&my_power)) {
+    Serial.print("Received Power: ");
+    Serial.println(int16_t(my_power));
+    setMotor(my_power);
+  }*/
+
   // Echo serial communications
-  // if(Serial.available()) {
-  //   digitalWrite(LED_1, HIGH);
-  //     Serial.write(Serial.read());
-  // }
+//   if(Serial.available()>0) {
+//     //digitalWrite(LED_1, HIGH);
+//     Serial.write(Serial.read());
+//     delay(10);
+//   }
   // digitalWrite(LED_1, LOW);
 
 
@@ -110,13 +133,14 @@ void loop(){
 //    tempData[7] = count & 0x000000ff;
 
     // Package analog read. Note that we divide by 4 to package into 8 bits
-    tempData[0] = (byte) (analogRead(A0) / 4);
-    int response = sendCANMessage(tempData, 8);
+    //tempData[0] = (byte) (analogRead(A0) / 4);
+    //tempData[0] = (byte) 200;  //Magic value to trick the code into thinking the winch is in a safe position for testing
+    /*int response = sendCANMessage(tempData, 8);
     if(response){
       Serial.println("Message sent");
     } else {
       Serial.println("Message fail");
-    }
+    }*/
   }
 }
 
@@ -167,12 +191,15 @@ int loopCAN(){
     // Determine if ID is standard (11 bits) or extended (29 bits)
     if((rxId & 0x80000000) == 0x80000000){
       if (rxId == 0x94ff0215 && len >= 2) {
-        winch.write(rxBuf[0]);
-        rudder.write(rxBuf[1]);
-        Serial.print("Setting winch, rudder: ");
+        winch.write(map(rxBuf[0], 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH));
+        rudder.write(map(rxBuf[1], 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH));
+        setMotor(rxBuf[2]-90);
+        Serial.print("Setting winch, rudder, ballast: ");
         Serial.print(rxBuf[0]);
         Serial.print(", ");
-        Serial.println(rxBuf[1]);
+        Serial.print(rxBuf[1]);
+        Serial.print(", ");
+        Serial.println(rxBuf[2]);
       }
     }
   }
@@ -193,16 +220,90 @@ int sendCANMessage(byte data[], int dataLength){
   }
 }
 
+void initMotor(uint8_t fwd_pin, uint8_t bwd_pin, uint8_t pwm_pin, uint8_t curr_pin, uint8_t fault1_pin, uint8_t fault2_pin) {
+  pinMode(fwd_pin, OUTPUT);
+  digitalWrite(fwd_pin, LOW);
+  pinMode(bwd_pin, OUTPUT);
+  digitalWrite(bwd_pin, LOW);
+  analogWrite(pwm_pin, 0);
+  pinMode(fault1_pin, INPUT);
+  pinMode(fault2_pin, INPUT);
+}
 
 
 
+void setMotor(int8_t power) {
+  if (power < 0) {
+    //Go Forward
+    digitalWrite(PIN_BWD, LOW);
+    digitalWrite(PIN_FWD, HIGH);
+    Serial.print("Writing Forward ");
+    Serial.println(uint8_t (abs(power)*2));
+    analogWrite(PIN_PWM, uint8_t(abs(power)*2));
+        
+  } else if (power>0) {
+    //Go Backward
+    digitalWrite(PIN_FWD, LOW);
+    digitalWrite(PIN_BWD, HIGH);
+    Serial.print("Writing Backward ");
+    Serial.println(uint8_t (abs(power)*2));
+    analogWrite(PIN_PWM, uint8_t(abs(power)*2));
+  } else {
+    //Brake/Coast
+    digitalWrite(PIN_FWD, LOW);
+    digitalWrite(PIN_BWD, LOW);
+    analogWrite(PIN_PWM, 0);
+    Serial.println("Writing Brake");
+  }
+}
 
-
-
-
-
-
-
-
+uint8_t parseInt(int16_t* val) {
+  static char state = 0;
+  static char negative = 0;
+  char c;  //temp buffer
+  while (Serial.available()>0) {
+    switch(state) {
+      case 0:  //Find the begin character
+        if(Serial.read() == 'p') {
+          negative = 0;
+          state = 1;
+          *val = 0;
+        }
+        break;
+      case 1:  //Parse the sign
+        if (Serial.peek() == '-') {
+          negative = 1;
+          Serial.read();  //Throw away the sign character
+        } else if (isDigit(Serial.peek())) {
+          state = 2; 
+        } else {
+          //Garbage data following p
+          Serial.read();
+          state = 0;
+        }
+        break;
+      case 2:  //Parse the digit
+        c = Serial.read();
+        if (isDigit(c)) {
+          *val = (*val)*10 + (c-'0');
+        } else if (c == '\n' || c=='\r') {
+          if (negative) {
+            *val = *val * -1;
+          }
+          state = 0;
+          return true;
+        } else {
+          //Garbage data
+          state = 0;
+        }
+        break;
+      default:
+        state = 0;
+        break;
+    }
+    delay(10);
+  }
+  return false;
+}
 
 
